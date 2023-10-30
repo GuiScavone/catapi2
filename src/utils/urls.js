6ba23430da70bfd70062d2eb4b1729d297de9666
Tatiana Taketsuma <tatiana.taketsuma@mindera.com>
2023-10-30 19:54:29 +0000

    addin utils folder

---
+++ src/utils/urls.js
@@ -0,0 +1,7 @@
+const baseUrl = 'https://api.thecatapi.com/v1/';
+
+export const endpoint = {
+    images: baseUrl + '/images/search',
+    favourites: baseUrl + '/favourites',
+    breeds: baseUrl + '/breeds',
+}
